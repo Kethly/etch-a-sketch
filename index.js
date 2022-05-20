0cce64c0ca266e20d2d68086f91fb2c944f0d7c8
Kethly <Kethlyfast@gmail.com>
2022-05-20 11:56:25 -0400

    finished product

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-let defaultGrid= 16;
-const generateBoard = rSize => {
+let defaultGrid= 18;
+let currentGrid = defaultGrid;
+let randomColors = false;
+const iterateRandom = val => val === true ? false : true;
+const clearBoard = () =>{
+    let container = document.querySelector("#board");
+    Array.from(container.children).forEach(child => container.removeChild(child));
+}
+const generateBoard = (rSize, randomColors = false) => {
+    if(!(+rSize >= 0)  || !(+rSize <= 100)){
+        alert("That's not valid!  It can only be between 0 to 100")
+        return;
+    }
+    clearBoard();
     let container = document.querySelector("#board");
     for(let x = 0; x < rSize; x++){
         let boardRow = document.createElement("div");
@@ -7,12 +19,18 @@ const generateBoard = rSize => {
         for(let i = 0; i < rSize; i++){
             let boardItem = document.createElement("div");
             boardItem.classList.add("boardItem");
-            boardItem.onmouseover = e => e.target.style.backgroundColor = "yellow";
+            if(randomColors === true){
+                let randomHex = "#" + parseInt(Math.random() * 16777215).toString(16);
+                boardItem.onmouseover = e => e.target.style.backgroundColor = randomHex;
+            } else {
+                boardItem.onmouseover = e => e.target.style.backgroundColor = "yellow";
+            }
             boardRow.appendChild(boardItem);
         }
         
         container.appendChild(boardRow);
     }
+    currentGrid = rSize;
 };
 window.onload = () => {
     generateBoard(defaultGrid);
